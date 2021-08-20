53585e0c86c1ef4dce97afcaec006120abf0e245
Sibints2000 <sibints2000@gmail.com>
2021-08-20 22:28:44 +0530

    Fourteenth Commit

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ with open("./Input/Letters/starting_letter.txt") as letter_file:
     letter_contents = letter_file.read()
     for name in names:
         stripped_name = name.strip()
-        new_letter = letter_contents.replace(PLACEHOLDER, name)
+        new_letter = letter_contents.replace(PLACEHOLDER, stripped_name)
+        print(new_letter)
 
 
 
