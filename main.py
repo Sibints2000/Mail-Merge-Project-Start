36ae4f03547ae8ed8f5631b0ff6f13f6f28e3a80
Sibints2000 <sibints2000@gmail.com>
2021-08-20 22:28:04 +0530

    Thirteenth Commit

--- main.py
+++ main.py
@@ -7,8 +7,9 @@ with open("./Input/Names/invited_names.txt") as names_file:
 with open("./Input/Letters/starting_letter.txt") as letter_file:
     letter_contents = letter_file.read()
     for name in names:
+        stripped_name = name.strip()
         new_letter = letter_contents.replace(PLACEHOLDER, name)
-        print(new_letter)
+
 
 
 
