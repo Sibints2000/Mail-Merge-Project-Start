19878c62e22537b6bf3c63f505051142e465aa21
Sibints2000 <sibints2000@gmail.com>
2021-08-20 22:26:29 +0530

    Twelfth Commit

--- main.py
+++ main.py
@@ -6,7 +6,9 @@ with open("./Input/Names/invited_names.txt") as names_file:
 
 with open("./Input/Letters/starting_letter.txt") as letter_file:
     letter_contents = letter_file.read()
-
+    for name in names:
+        new_letter = letter_contents.replace(PLACEHOLDER, name)
+        print(new_letter)
 
 
 
