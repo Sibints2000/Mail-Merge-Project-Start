45dec42f578b93332723002554c6805beea4202a
Sibints2000 <sibints2000@gmail.com>
2021-08-20 22:32:03 +0530

    Sixteenth Commit

--- main.py
+++ main.py
@@ -9,7 +9,8 @@ with open("./Input/Letters/starting_letter.txt") as letter_file:
     for name in names:
         stripped_name = name.strip()
         new_letter = letter_contents.replace(PLACEHOLDER, stripped_name)
-        print(new_letter)
+        with open(f"./Output/ReadyToSend/letter_for_{stripped_name}.txt", mode="w") as completed_letter:
+            completed_letter.write()
 
 
 # for each name in invited_names.txt
